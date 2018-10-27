8f3057dbd05ba5ac18a46ee7ea7b796e1309c2de
lalrishav <lalrishav@gmail.com>
2018-10-28 01:25:49 +0530

    initialising

---
+++ server.js
@@ -0,0 +1,9 @@
+require('rtcmulticonnection-server')();
+console.log("hello")
+console.log("hw r u");
+/*
+require('rtcmulticonnection-server')({
+    config: __dirname + resolveURL('/config.json'),
+    logs: __dirname + resolveURL('/logs.json')
+});
+*/
